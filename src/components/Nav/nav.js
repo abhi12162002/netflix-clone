de1faa0763cd5bbb52a093f8de591314cc1f5aa7
Abhishek <abhisolanky12162002@gmail.com>
2022-07-31 22:16:31 +0530

    first commit

---
+++ src/components/Nav/nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./nav.css"
+const Nav = () => {
+   
+   
+
+
+  return (
+    <div className="nav">
+    <img className='nav_logo' src="https://www.edigitalagency.com.au/wp-content/uploads/netflix-logo-png-large.png" alt="Netflix Logo" />
+      
+      <img className='nav_avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="Netflix Logo" />
+    </div>
+  )
+}
+
+export default Nav;
